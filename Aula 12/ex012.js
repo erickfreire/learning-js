f0cdfa43d56c8bf9e8326a63bfc8abc06f4e035b
Erick <erickfsbmantel@outlook.com>
2024-12-26 14:54:36 -0300

    commit 8

---
+++ Aula 12/ex012.js
@@ -0,0 +1,10 @@
+var hora = 20
+
+console.log(`Agora são exatamente ${hora} horas.`)
+if (hora < 12){
+    console.log('Bom Dia!')
+}else if (hora <= 18){
+    console.log('Boa Tarde!')
+}else{
+    console.log('Boa Noite!')
+}
